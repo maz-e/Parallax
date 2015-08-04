08958450a66afaadad47436fee9789771180e202
Miguel Angel <m.zapes@gmail.com>
2015-08-04 11:34:01 +0200

    Delete files
    Erase innecessary files for the module

--- mod_parallax/mod_parallax.php
+++ mod_parallax/mod_parallax.php
@@ -9,8 +9,5 @@
 
 // No direct access
 defined('_JEXEC') or die;
-// Include the syndicate functions only once
-//require_once dirname(__FILE__) . '/helper.php';
 
-//$parallax = modParallaxHelper::getParallax($params);
 require JModuleHelper::getLayoutPath('mod_parallax');
