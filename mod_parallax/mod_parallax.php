d3b81303b4f9703607147e703f6e32a44aa48426
Miguel Angel <m.zapes@gmail.com>
2015-08-04 10:48:08 +0200

    Add basic code
    Basic code for the module

---
+++ mod_parallax/mod_parallax.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  mod_parallax
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+//require_once dirname(__FILE__) . '/helper.php';
+
+//$parallax = modParallaxHelper::getParallax($params);
+require JModuleHelper::getLayoutPath('mod_parallax');
---
+++ mod_parallax/tmpl/default.php
@@ -0,0 +1,7 @@
+<?php
+// No direct access
+defined('_JEXEC') or die;
+?>
+<div class="parallax">
+   <img src="">
+</div>
